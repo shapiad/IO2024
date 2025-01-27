58e161bbd436d538db24e7db6f6c5ebf28e9c077
shapiad <shapiad@mat.umk.pl>
2025-01-27 04:17:44 +0100

    Fibonacci

--- program.cpp
+++ program.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
     int num;
     cin >> num;
-    cout << num << endl;
+    cout << "Number: " << num << endl;
+    cout << "Fibonacci: " << fibonacci(num) << endl;
     return 0;
 }
+
+
+
+
