3d42dd16334e592e73bbf44d3e6622e1fdef6ac2
shapiad <shapiad@mat.umk.pl>
2025-01-27 04:19:05 +0100

    Add validation

--- program.cpp
+++ program.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     int num;
     cin >> num;
+    if (num < 0) {
+        cout << "Error: Negative number" << endl;
+        return 1;
+    }
     cout << num << endl;
     return 0;
 }
